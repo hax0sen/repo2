cb9044a527145f2f9d06bb180beb3bf081c4cebc
hax0sen <60272204+hax0sen@users.noreply.github.com>
2024-10-11 00:00:28 +0200

    Update V1_0__create_snus_table.sql

--- db/V1_0__create_snus_table.sql
+++ db/V1_0__create_snus_table.sql
@@ -1,7 +1,6 @@
--- Create employees table
+-- Create snus table
 CREATE TABLE IF NOT EXISTS snus (
     snus_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL
 );
