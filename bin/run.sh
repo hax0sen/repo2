c76927ea699404c0c1998ca52352b75b0eeddf1d
hax0sen <60272204+hax0sen@users.noreply.github.com>
2024-10-10 10:28:43 +0200

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-dddocker --version
+
+# Access the credentials via environment variables injected by Jenkins
+echo "Using Sonar Token: $SONAR_TOKEN"
+echo "Using Nexus Token: $NEXUS_TOKEN"
+echo "Nexus Username: $NEXUS_USER"
+echo "Nexus Password: $NEXUS_PASS"
+
+# Echo Jenkins agent environment variables
+echo "Jenkins agent proxy: $vn_proxy"  # Assuming 'vn-proxy' was a typo and you meant 'vn_proxy'
+
+# You can print all the environment variables for debugging purposes
+echo "==== Jenkins Agent Environment Variables ===="
+env
+
+# Your logic using these credentials goes here...
