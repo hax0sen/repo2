dcb74cd4db8ff01d0b5ad6516f42e5e9b80d1477
hax0sen <60272204+hax0sen@users.noreply.github.com>
2024-10-10 23:29:27 +0200

    fdd

--- db/V1_0__create_emp_table.sql
+++
@@ -1,8 +0,0 @@
--- Create employees table
-CREATE TABLE IF NOT EXISTS employees (
-    employee_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    hire_date DATE NOT NULL
-);
